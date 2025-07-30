dbdd0ef6bbd9dae850bd54767772a18b8cd126ee
dheeri1 <dheerendra.singh@evervent.in>
2025-07-30 14:49:04 +0530

    update vite config file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,24 +1,12 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-// import path from 'path';
-// import { fileURLToPath } from 'url';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// // Convert the module URL to a file path
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-
-// export default defineConfig({
-//   plugins: [react()],
-//   resolve: {
-//     alias: {
-//       '@': path.resolve(__dirname, './src'),
-//     },
-//   },
-// });
-
-export default {
-  build: {
-    sourcemap: false,
-    minify: false,
-  }
-}
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
